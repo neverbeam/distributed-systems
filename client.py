37ca71557305e0d6da2fc4a3185d4553b8883192
Patrick Wernke <patrick@bit-students.com>
2018-11-16 15:45:25 +0100

    Python3 client server example

--- client.py
+++ client.py
@@ -1,12 +1,34 @@
 
-import socket
+# socket_echo_client.py
 
-if __name__ == "__main__":
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("localhost", 9000))
-    myid = 1
-    data = myid
-    sock.sendall(data)
-    result = sock.recv(1024)
-    print result
-sock.close()
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 10000)
+print('connecting to {} port {}'.format(*server_address))
+sock.connect(server_address)
+
+try:
+
+    # Send data
+    message = b'This is the message.  It will be repeated.'
+    print('sending {!r}'.format(message))
+    sock.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print('received {!r}'.format(data))
+
+finally:
+    print('closing socket')
+    sock.close()
+
--- server.py
+++ server.py
@@ -1,60 +1,38 @@
-import multiprocessing
-import socket
+# socket_echo_server.py
 
-def handle(connection, address):
-    import logging
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger("process-%r" % (address,))
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('localhost', 10000)
+print('starting up on {} port {}'.format(*server_address))
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
     try:
-        logger.debug("Connected %r at %r", connection, address)
+        print('connection from', client_address)
+
+        # Receive the data in small chunks and retransmit it
         while True:
-            data = connection.recv(1024)
-            if data == "":
-                logger.debug("Socket closed remotely")
+            data = connection.recv(16)
+            print('received {!r}'.format(data))
+            if data:
+                print('sending data back to the client')
+                connection.sendall(data)
+            else:
+                print('no data from', client_address)
                 break
-            logger.debug("Received data %r", data)
-            connection.sendall(data)
-            logger.debug("Sent data")
-    except:
-        logger.exception("Problem handling request")
+
     finally:
-        logger.debug("Closing socket")
+        # Clean up the connection
         connection.close()
 
-class Server(object):
-    def __init__(self, hostname, port):
-        import logging
-        self.logger = logging.getLogger("server")
-        self.hostname = hostname
-        self.port = port
-
-    def start(self):
-        self.logger.debug("listening")
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.hostname, self.port))
-        self.socket.listen(1)
-
-        while True:
-            conn, address = self.socket.accept()
-            self.logger.debug("Got connection")
-            process = multiprocessing.Process(target=handle, args=(conn, address))
-            process.daemon = True
-            process.start()
-            self.logger.debug("Started process %r", process)
-
-if __name__ == "__main__":
-    import logging
-    logging.basicConfig(level=logging.DEBUG)
-    server = Server("0.0.0.0", 9000)
-    try:
-        logging.info("Listening")
-        server.start()
-    except:
-        logging.exception("Unexpected exception")
-    finally:
-        logging.info("Shutting down")
-        for process in multiprocessing.active_children():
-            logging.info("Shutting down process %r", process)
-            process.terminate()
-            process.join()
-logging.info("All done")

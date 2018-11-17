ac5a02dfd717ec89069102ad048bf24b977ce6e2
neverbeam <martijndortmond@hotmail.com>
2018-11-17 16:09:05 +0100

    Updated client with thread to send and receive on the same time without blocking

--- client.py
+++ client.py
@@ -2,30 +2,51 @@
 # socket_echo_client.py
 
 import socket
-import sys
+import select
+from threading import Thread
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def connect():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
-print('connecting to {} port {}'.format(*server_address))
-sock.connect(server_address)
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 10000)
+    print('connecting to {} port {}'.format(*server_address))
+    sock.connect(server_address)
+
+    return sock
 
 
-def sendmessage(message):
+def sendmessage(message, sock):
         # Send data
         print('sending {!r}'.format(message))
         sock.sendall(message)
 
-        # receive  data
+
+def player_moves(sock):
+    message = b'This is the message.  It will be repeated.'
+    sendmessage(message, sock)
+
+def server_input(sock):
+    while True:
+        readable, writable, errored = select.select([sock], [sock], [])
         data = sock.recv(64)
-        print('received {!r}'.format(data))
-
-message = b'This is the message.  It will be repeated.'
-
-sendmessage(message)
+        if data:
+            # update my update_grid
+            print ("updating grid")
 
 
-print('closing socket')
-sock.close()
+
+
+if __name__ == "__main__":
+    # Connect to the host
+    sock = connect() # perhaps argument port
+
+    # Thread for doing moves + send moves
+    Thread(target=player_moves, args = (sock,)).start()
+
+    # Receive input from servers
+    server_input(sock)
+
+    print('closing socket')
+    sock.close()

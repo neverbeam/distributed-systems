057abdf8d76bef6a510d6189cdc186a36bc9da36
neverbeam <martijndortmond@hotmail.com>
2018-11-20 22:49:47 +0100

    Now sends and parse joins

--- client.py
+++ client.py
@@ -3,11 +3,44 @@ import select
 from threading import Thread
 
 
+class gridobject:
+    def __init__(self, data):
+        self.name = data[0]
+        self.x = data[1]
+        self.y = data[2]
+        self.hp = data[3]
+        self.ap = data[4]
+
+        print ("made player with: \n name:{} \n x: {} \n y:{} \n hp: {} \n ap: {}".format(self.name, self.x, self.y, self.hp, self.ap))
+
+
 class Client:
     def __init__(self, port=10000):
         self.sock = self.connect_server(port=port)
 
+    def recieve_grid(self, sock):
+        """ Receive the current state of the grid from the server. """
+        grid = {}
+        data = ""
+        # Keep receiving until an end has been send. TCP gives in order arrival
+        while True:
+            data += sock.recv(128).decode('utf-8')
+            if data[-3:] == "end":
+                break
+
+        #Parse the data so that the user contains the whole grid.
+        data = data.split(";")
+        del data[-1]
+        for i in range(0, len(data), 5):
+            playerdata = data[i:i+5]
+            player = gridobject(playerdata)
+            grid[player.name] = player
+
+        print ( "succesfully received grid")
+
+
     def connect_server(self, port=10000):
+        """ Connect to the server. """
         # Create a TCP/IP socket
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -15,33 +48,49 @@ class Client:
         server_address = ('localhost', port)
         print('connecting to {} port {}'.format(*server_address))
         sock.connect(server_address)
+        self.recieve_grid(sock)
 
         return sock
 
     def disconnect_server(self):
+        """ disconnect from the server"""
         # close the socket
         self.sock.close()
 
     def send_message(self, message):
+        """ Send an message/action to the server"""
         # Send data
         print('sending {!r}'.format(message))
         self.sock.sendall(message)
 
     def start_moving(self):
-        # Thread for doing moves + send moves
+        """Thread for doing moves + send moves"""
         Thread(target=self.player_moves, args=()).start()
 
     def player_moves(self):
-        message = b'This is the message.  It will be repeated.'
-        self.send_message(message)
+        """ User input function. """
+        while True:
+            message = input("Create an action")
+            self.send_message(message.encode('utf-8'))
+
+    def update_grid(self,data):
+        """ Update my grid. """
+
+        thedata = data.split(";")
+        if thedata[0] == "join":
+            player = gridobject(thedata[1:])
+            grid[player.name] = player
+
 
     def server_input(self):
+        """ Check for server input. """
         while True:
             readable, writable, errored = select.select([self.sock], [self.sock], [])
             data = self.sock.recv(64)
             if data:
                 # update my update_grid
-                print ("updating grid")
+                self.update_grid(data)
+                print ("message: " + data.decode('utf-8'))
 
 
 
@@ -54,5 +103,5 @@ if __name__ == "__main__":
     # Receive input from servers
     c.server_input()
 
-    print('closing socket')
+    print('closing socket') # probably requires try except keyboard interrupt
     c.disconnect_server()
--- server.py
+++ server.py
@@ -1,11 +1,27 @@
 import socket
 import select
+import random
+import time
+
+class gridobject:
+    """ Defines a grid object on the grid. Could be player or dragon"""
+    def __init__(self, name):
+        self.x = random.randint(0,25)
+        self.y = random.randint(0,25)
+        self.hp = 10
+        self.ap = 5
+        self.cd = 0
+        self.name = name
+
 
 class Server:
     def __init__(self, port=10000):
         self.start_up(port)
+        # we could also place object on a 25x25 grid
+        self.grid = {}
 
     def start_up(self, port=10000):
+        """ Create an server for das game. """
         # Create a TCP/IP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,35 +35,59 @@ class Server:
         self.sock.listen(3)
 
     def power_down(self):
+        """ Close down the server. """
         print ("Terminating and closing existing connections")
         for connection in self.connections:
             connection.close()
 
     def update_grid(self, data):
-        # placeholder
-        print ("updated")
+        """Update my grid and pass on the message"""
+        # Requires server parsing here
+        for clients in self.connections[1:]:
+            clients.sendall(data)
+
+    def send_grid(self, client):
+        """Send the grid to new players"""
+
+        for player in self.grid.values():
+            data = "{};{};{};{};{};".format(player.name, player.x, player.y, player.hp, player.ap)
+            client.sendall(data.encode('utf-8'))
+        # Send ending character
+        client.sendall(b"end")
+        print("finished sending grid")
+
+    def create_player(self, client):
+        """Create a player and message this to every body else."""
+        player = gridobject(len(self.grid))
+        self.grid[client] = player
+        self.send_grid(client)
+
+        # Send data to other clients
+        data = "join;{};{};{};{};{}".format(player.name, player.x, player.y, player.hp, player.ap)
+        for clients in self.connections[1:]:
+            clients.sendall(data.encode('utf-8'))
+
 
     def read_ports(self):
+        """ Read the sockets for new connections or player noticeses."""
         while True:
             try:
                 # Wait for a connection
-                print('waiting for a connection')
                 readable, writable, errored = select.select(self.connections, [], [])
 
                 for client in readable:
                     # If server side, then new connection
                     if client is self.sock:
                         connection, client_address = self.sock.accept()
-                        self.connections.append(connection)
                         print ("Someone connected from {}".format(client_address))
+                        self.create_player(connection)
+                        self.connections.append(connection)
                     # Else we have some data
                     else:
                         data = client.recv(64)
                         print('received {!r}'.format(data))
                         if data:
-                            print('sending data back to the client')
                             update = self.update_grid(data) # perhaps do this in another thread
-                            client.sendall(data)  # Perhaps do a broadcast here.
                         else: #connection has closed
                             print ("closing the connection")
                             self.connections.remove(client)
